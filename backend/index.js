62645d4fc344de5e32ec41cea062a0fdd117732d
skrijwan <skrijwan329@gmail.com>
2024-11-18 03:16:11 +0530

    core fix

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 app.use(cors({
-  origin: 'https://notebook-mern.vercel.app',  // Allow requests only from frontend
+  origin: 'https://notebook-mern-pi8g.vercel.app',  // Allow requests only from frontend
   // origin: 'http://localhost:3000',  // Allow requests only from frontend
   methods: 'GET, POST, PUT, DELETE', // Allow specific methods
   allowedHeaders: 'Content-Type, Authorization, auth-token', // Add allowed headers
